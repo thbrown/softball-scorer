3229a960aebf0b08bbd787de8bde7082fa7365a1
Thomas Brown <thomasaustenbrown@gmail.com>
2018-07-05 01:16:37 -0600

    Make stats sortable

--- src/card-player-list.js
+++ src/card-player-list.js
@@ -5,77 +5,105 @@ const DOM = require( 'react-dom-factories' );
 
 const state = require( 'state' );
 
+let sortField = "Name";
+let sortDirection = "ASC";
+
 module.exports = class CardPlayerList extends expose.Component {
 	constructor( props ) {
 		super( props );
 		this.expose();
-		this.state = {};
+		this.state = {
+			sortField: sortField,
+			sortDirection: sortDirection
+		};
+
+		this.handleStatsClick = function(a) {
+			sortField = a;
+			if(this.state.sortDirection === "DSC") {
+				sortDirection = "ASC";
+			} else {
+				sortDirection = "DSC";
+			}
+			// TODO check if state is asc or dsc
+
+			this.setState({
+				sortField: sortField,
+				sortDirection: sortDirection
+			} );
+		}.bind( this );
 	}
 
 	renderPlayerList(){
 		const s = state.getState();
-		let players = s.players.filter( ( player ) => {
+		let playerStats = s.players.filter( ( player ) => {
 			return this.props.team.games.reduce( ( result, game ) => {
 				return result || game.lineup.indexOf( player.id ) > -1;
 			}, false );
 		} ).map( ( player ) => {
-			let stats = state.buildStatsObject(this.props.team.id, player.id);
+			return state.buildStatsObject(this.props.team.id, player.id);
+		} ).sort( ( a, b ) => {
+			if(this.state.sortDirection === "DSC") {
+				return a[this.state.sortField] < b[this.state.sortField];
+			} else {
+				return a[this.state.sortField] > b[this.state.sortField];
+			}
+		} ).map( ( playerStats ) => {
+			
 			return DOM.div( {
-				player_id: player.id,
-				key: 'player' + player.id,
+				player_id: playerStats.id,
+				key: 'player' + playerStats.id,
 				className: 'table-row',
-				//onClick: this.handleButtonClick.bind( this, team )
 			},
 				DOM.div( {
 					style: {
 						width: '100px'
 					}
-				}, player.name ),
+				}, playerStats.name ),
 				DOM.div( {
 					className: 'percentage-stat-cell',
 					style: {
 					}
-				}, stats.battingAverage ),
+				}, playerStats.battingAverage ),
 				DOM.div( {
 					className: 'percentage-stat-cell',
 					style: {
 					}
-				}, stats.sluggingPercentage ),
+				}, playerStats.sluggingPercentage ),
 				DOM.div( {
 					className: 'number-stat-cell',
 					style: {
 					}
-				}, stats.atBats ),
+				}, playerStats.atBats ),
 				DOM.div( {
 					className: 'number-stat-cell',
 					style: {
 					}
-				}, stats.doubles ),
+				}, playerStats.doubles ),
 				DOM.div( {
 					className: 'number-stat-cell',
 					style: {
 					}
-				}, stats.triples ),
+				}, playerStats.triples ),
 				DOM.div( {
 					className: 'number-stat-cell',
 					style: {
 					}
-				}, stats.insideTheParkHR ),
+				}, playerStats.insideTheParkHR ),
 				DOM.div( {
 					className: 'number-stat-cell',
 					style: {
 					}
-				}, stats.outsideTheParkHR ),
+				}, playerStats.outsideTheParkHR ),
 				DOM.div( {
 					className: 'number-stat-cell',
 					style: {
 					}
-				}, stats.walks ),
+				}, playerStats.walks ),
 				DOM.div( {
 					className: 'number-stat-cell',
 					style: {
 					}
-				}, stats.reachedOnError )
+				}, playerStats.reachedOnError )
 			);
 		} );
 
@@ -83,51 +111,61 @@ module.exports = class CardPlayerList extends expose.Component {
 					key: 'header',
 					className: 'table-row',
 				}, DOM.span( {
+					onClick: this.handleStatsClick.bind( this, "name" ),
 					style: {
 						width: '100px'
 					}
 				}, "Name" ),
 				DOM.span( {
+					onClick: this.handleStatsClick.bind( this, "battingAverage" ),
 					className: 'percentage-stat-cell-header',
 					style: {
 					}
 				}, "BA" ),
 				DOM.span( {
+					onClick: this.handleStatsClick.bind( this, "sluggingPercentage" ),
 					className: 'percentage-stat-cell-header',
 					style: {
 					}
 				}, "SLG" ),
 				DOM.span( {
+					onClick: this.handleStatsClick.bind( this, "atBats" ),
 					className: 'number-stat-cell-header',
 					style: {
 					}
 				}, "AB" ),
 				DOM.div( {
+					onClick: this.handleStatsClick.bind( this, "doubles" ),
 					className: 'number-stat-cell-header',
 					style: {
 					}
 				}, "2B" ),
 				DOM.div( {
+					onClick: this.handleStatsClick.bind( this, "triples" ),
 					className: 'number-stat-cell-header',
 					style: {
 					}
 				}, "3B" ),
 				DOM.div( {
+					onClick: this.handleStatsClick.bind( this, "insideTheParkHR" ),
 					className: 'number-stat-cell-header',
 					style: {
 					}
 				}, "HRI" ),
 				DOM.div( {
+					onClick: this.handleStatsClick.bind( this, "outsideTheParkHR" ),
 					className: 'number-stat-cell-header',
 					style: {
 					}
 				}, "HRO" ),
 				DOM.div( {
+					onClick: this.handleStatsClick.bind( this, "walks" ),
 					className: 'number-stat-cell-header',
 					style: {
 					}
 				}, "BB" ),
 				DOM.div( {
+					onClick: this.handleStatsClick.bind( this, "reachedOnError" ),
 					className: 'number-stat-cell-header',
 					style: {
 					}
@@ -135,7 +173,7 @@ module.exports = class CardPlayerList extends expose.Component {
 
 		let elems = [];
 		elems.push(header);
-		elems.push(players);
+		elems.push(playerStats);
 
 		return DOM.div( {
 
