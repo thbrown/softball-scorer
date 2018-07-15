f5a118a1a5753e1abf17831515d16a217022504c
Thomas Brown <thomasaustenbrown@gmail.com>
2018-07-15 17:58:58 -0600

    Add ui to create account and reset password

---
+++ src/card-signup.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const expose = require( './expose' );
+const DOM = require( 'react-dom-factories' );
+const css = require( 'css' );
+
+const dialog = require( 'dialog' );
+const state = require( 'state' );
+
+module.exports = class CardSignup extends expose.Component {
+	constructor( props ) {
+		super( props );
+		this.expose();
+		this.state = {};
+
+		this.handleBackClick = function() {
+			expose.set_state( 'main', {
+				page: 'Auth'
+			} );
+		};
+
+		this.handleSubmitClick = function() {
+			let email = document.getElementById( 'email' );
+			let password = document.getElementById( 'password' );
+			let passwordConfirm = document.getElementById( 'passwordConfirm' );
+
+			if ( email.value && password.value && passwordConfirm.value) {
+				if(password.value !== passwordConfirm.value) {
+					dialog.show_notification('Passwords do not match');
+					return;
+				}
+				dialog.show_notification('This functionality has not yet been implemented');
+			} else {
+				let map = {
+					"Email": email.value,
+					"Password": password.value,
+					"Confirm Password": passwordConfirm.value
+				}
+				let missingFields = Object.keys(map).filter(field => {
+					return !map[field];
+				})
+				console.log(missingFields);
+				dialog.show_notification('Please fill out the following required fields: ' + missingFields.join(','));
+			}
+		};
+	}
+
+	renderAuthInterface() {
+		return DOM.div( {
+			className: 'auth-input-container',
+		},
+		DOM.input( {
+			key: 'email',
+			id: 'email',
+			className: 'auth-input',
+			placeholder: 'Email',
+			type: 'email'
+		} ),
+		DOM.input( {
+			key: 'password',
+			id: 'password',
+			className: 'auth-input',
+			placeholder: 'Password',
+			type: 'password',
+		} ),
+		DOM.input( {
+			key: 'passwordConfirm',
+			id: 'passwordConfirm',
+			className: 'auth-input',
+			placeholder: 'Confirm Password',
+			type: 'password',
+		} ),
+		// TODO: capcha
+		this.renderSubmitButton(),
+		);
+	}
+
+	renderSubmitButton() {
+		return DOM.div( {
+			key: 'submit',
+			id: 'submit',
+			className: 'button confirm-button',
+			onClick: this.handleSubmitClick,
+			style: {
+				marginLeft: '0'
+			}
+		}, 'Submit');
+	}
+
+	render() {
+		return DOM.div( {
+				style: {}
+			},
+			DOM.div( {
+					className: 'card-title'
+				},
+				DOM.img( {
+					src: 'assets/ic_arrow_back_white_36dp_1x.png',
+					className: 'back-arrow',
+					onClick: this.handleBackClick,
+				} ),
+				DOM.div( {
+					style: {
+					}
+				}, 'Signup' )
+			),
+			this.renderAuthInterface()
+		);
+	}
+};
