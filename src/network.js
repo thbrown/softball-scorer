5368efd8155ce1a07548389fe877d0a0819a131b
benjamin-t-brown <benjamin.t.brown@hotmail.com>
2018-10-03 06:58:26 -0600

    fixed some formatting

--- src/network.js
+++ src/network.js
@@ -1,65 +1,63 @@
 'use strict';
 
-exports.request = async function(method, url, body) {
+exports.request = async function( method, url, body ) {
+	url = exports.getServerUrl( url );
 
-	url = exports.getServerUrl(url);
+	console.log( "Request to ", url );
 
-	console.log("Request to ", url);
-
-	let response = {};
-	if('fetch' in window) {
-		let res = await fetch(url, {
+	const response = {};
+	if ( 'fetch' in window ) {
+		const res = await fetch( url, {
 			method: method,
 			credentials: 'same-origin',
 			headers: {
 				'content-type': 'application/json'
 			},
 			body: body,
-		})
+		} );
 		response.status = res.status;
-		if(response.status !== 204) {
+		if ( response.status !== 204 ) {
 			try {
 				response.body = await res.json();
-			} catch (e) {
-				console.log(e);
+			} catch ( e ) {
+				console.log( e );
 			}
 		}
 	} else {
-		console.log("Using XMLHttpRequest");
 		// TODO: This is untested and probably doesn't work
-		let request = await new Promise(function (resolve, reject) {
-	        let xhr = new XMLHttpRequest();
+		const request = await new Promise( function( resolve, reject ) {
+			const xhr = new XMLHttpRequest();
 			xhr.open( method, url, true );
 			xhr.setRequestHeader( 'Content-type', 'application/json' );
-	        xhr.onload = function () {
-	            if (this.status >= 200 && this.status < 300) {
-	                resolve(xhr.response);
-	            } else {
-	                reject({
-	                    status: this.status,
-	                    statusText: xhr.statusText,
-	                    response: xhr.response
-	                });
-	            }
-	        };
-	        xhr.onerror = function () {
-	            reject({
-	                status: this.status,
-	                statusText: xhr.statusText
-	            });
-	        };
-	        xhr.send( JSON.stringify( body ) );
-	    });
+			xhr.onload = function() {
+				if ( this.status >= 200 && this.status < 300 ) {
+					resolve( xhr.response );
+				} else {
+					reject( {
+						status: this.status,
+						statusText: xhr.statusText,
+						response: xhr.response
+					} );
+				}
+			};
+			xhr.onerror = function() {
+				reject( {
+					status: this.status,
+					statusText: xhr.statusText
+				} );
+			};
+			xhr.send( JSON.stringify( body ) );
+		} );
 		response.status = request.status;
-		if(response.status !== 204) {
+		if ( response.status !== 204 ) {
 			response.body = request.response;
 		}
 	}
-	console.log("Request Complete", url, response.status);
+	console.log( "Request Complete", url, response.status );
 	return response;
 };
 
-exports.getServerUrl = function(path) {
+exports.getServerUrl = function( path ) {
 	return window.location.origin + "/" + path;
 };
 
