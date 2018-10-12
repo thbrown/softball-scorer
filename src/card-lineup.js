ba7a45565d24aaa9d64eddf419331b4e38a60f98
Thomas Brown <thomasaustenbrown@gmail.com>
2018-10-11 22:26:48 -0600

    Fix issue with linup removal button styling

--- src/card-lineup.js
+++ src/card-lineup.js
@@ -379,7 +379,7 @@ module.exports = class CardLineup extends expose.Component {
 			elems.push( DOM.img( {
 				key: 'del',
 				src: '/assets/remove.svg',
-				className: 'list-button',
+				className: 'lineup-row-button',
 				style: {
 					paddingTop: '20px',
 					paddingBottom: '20px',
