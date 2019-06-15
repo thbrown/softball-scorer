f7ea9e8ab7c9e7f303bec80cfa2de3c5b0ff8c2f
Thomas Brown <thomasaustenbrown@gmail.com>
2019-06-15 10:24:10 -0600

    Add some changes to fix in-memory db, but it's still broken

--- src-srv/database-calls-static.js
+++ src-srv/database-calls-static.js
@@ -1,5 +1,8 @@
-const objectMerge = require("../object-merge.js");
-const HandledError = require("./handled-error.js");
+const objectMerge = require('../object-merge.js');
+const HandledError = require('./handled-error.js');
+
+const idUtils = require('../id-utils.js');
+const logger = require('./logger.js');
 
 let databaseCalls = class DatabaseCalls {
   constructor() {
@@ -7,256 +10,256 @@ let databaseCalls = class DatabaseCalls {
     this.ACCOUNTS = [
       {
         account_id: 1,
-        email: "test@softball.app",
+        email: 'test@softball.app',
         password_hash:
-          "$2b$12$pYo/XmmYN27OK08.ZyNqtealmhaFRfg6TgIHbuTJFbAiNO7M2rwb2", // pizza
-        password_token_hash: "abcdefg",
-        password_token_expiration: Date.now() + 3600000
-      }
+          '$2b$12$pYo/XmmYN27OK08.ZyNqtealmhaFRfg6TgIHbuTJFbAiNO7M2rwb2', // pizza
+        password_token_hash: 'abcdefg',
+        password_token_expiration: Date.now() + 3600000,
+      },
     ];
     this.STATES = {
       1: {
         optimizations: [],
         players: [
           {
-            id: "4f4CQExEegoHbV",
-            name: "Harry",
-            gender: "M",
+            id: '4f4CQExEegoHbV',
+            name: 'Harry',
+            gender: 'M',
             song_link: null,
-            song_start: null
+            song_start: null,
           },
           {
-            id: "4yIOlHpfiREBfJ",
-            name: "Ron",
-            gender: "M",
+            id: '4yIOlHpfiREBfJ',
+            name: 'Ron',
+            gender: 'M',
             song_link: null,
-            song_start: null
+            song_start: null,
           },
           {
-            id: "4SFlePddQQh8OY",
-            name: "Hermione",
-            gender: "F",
+            id: '4SFlePddQQh8OY',
+            name: 'Hermione',
+            gender: 'F',
             song_link: null,
-            song_start: null
+            song_start: null,
           },
           {
-            id: "47guOwUQN5QLPc",
-            name: "Gina",
-            gender: "F",
+            id: '47guOwUQN5QLPc',
+            name: 'Gina',
+            gender: 'F',
             song_link: null,
-            song_start: null
+            song_start: null,
           },
           {
-            id: "4ZqtZbpYtlxOVW",
-            name: "Carlos",
-            gender: "M",
+            id: '4ZqtZbpYtlxOVW',
+            name: 'Carlos',
+            gender: 'M',
             song_link: null,
-            song_start: null
+            song_start: null,
           },
           {
-            id: "4ePGkCgKNTSnH4",
-            name: "Jewels",
-            gender: "F",
+            id: '4ePGkCgKNTSnH4',
+            name: 'Jewels',
+            gender: 'F',
             song_link: null,
-            song_start: null
-          }
+            song_start: null,
+          },
         ],
         teams: [
           {
-            id: "4jouU8AOMIpbj",
-            name: "Big Guns",
+            id: '4jouU8AOMIpbj',
+            name: 'Big Guns',
             games: [
               {
-                id: "4HFlIjRu8XBV0I",
-                opponent: "Bad Guys",
-                lineup: ["4f4CQExEegoHbV", "4yIOlHpfiREBfJ", "4SFlePddQQh8OY"],
-                date: "2018-10-10",
-                park: "Stazio",
+                id: '4HFlIjRu8XBV0I',
+                opponent: 'Bad Guys',
+                lineup: ['4f4CQExEegoHbV', '4yIOlHpfiREBfJ', '4SFlePddQQh8OY'],
+                date: '2018-10-10',
+                park: 'Stazio',
                 scoreUs: 0,
                 scoreThem: 0,
                 lineupType: 2,
                 plateAppearances: [
                   {
-                    id: "4K7eqMusZ9flF",
-                    player_id: "4f4CQExEegoHbV"
+                    id: '4K7eqMusZ9flF',
+                    player_id: '4f4CQExEegoHbV',
                   },
                   {
-                    id: "4kl5b2Aa59lkJw",
-                    player_id: "4yIOlHpfiREBfJ",
+                    id: '4kl5b2Aa59lkJw',
+                    player_id: '4yIOlHpfiREBfJ',
                     location: {
                       x: 12141,
-                      y: 12141
-                    }
-                  },
-                  {
-                    id: "4tlFhCnQ79KTyV",
-                    player_id: "4SFlePddQQh8OY",
-                    result: "2B",
-                    location: {
-                      x: 12141,
-                      y: 12141
-                    }
-                  },
-                  {
-                    id: "4h2ZbOePr2inJ",
-                    player_id: "4f4CQExEegoHbV",
-                    location: {
-                      x: 12141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "Out"
                   },
                   {
-                    id: "4MkyWloxw2cMqL",
-                    player_id: "4yIOlHpfiREBfJ",
+                    id: '4tlFhCnQ79KTyV',
+                    player_id: '4SFlePddQQh8OY',
+                    result: '2B',
                     location: {
                       x: 12141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "SAC"
                   },
                   {
-                    id: "40fF3EKZVAqktS",
-                    player_id: "4SFlePddQQh8OY",
+                    id: '4h2ZbOePr2inJ',
+                    player_id: '4f4CQExEegoHbV',
                     location: {
                       x: 12141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "E"
-                  }
-                ]
+                    result: 'Out',
+                  },
+                  {
+                    id: '4MkyWloxw2cMqL',
+                    player_id: '4yIOlHpfiREBfJ',
+                    location: {
+                      x: 12141,
+                      y: 12141,
+                    },
+                    result: 'SAC',
+                  },
+                  {
+                    id: '40fF3EKZVAqktS',
+                    player_id: '4SFlePddQQh8OY',
+                    location: {
+                      x: 12141,
+                      y: 12141,
+                    },
+                    result: 'E',
+                  },
+                ],
               },
               {
-                id: "4Ju8hnXrGUPtIk",
-                opponent: "Bad Guys 2",
-                lineup: ["4f4CQExEegoHbV", "4yIOlHpfiREBfJ", "4SFlePddQQh8OY"],
-                date: "2018-10-10",
-                park: "Stazio",
+                id: '4Ju8hnXrGUPtIk',
+                opponent: 'Bad Guys 2',
+                lineup: ['4f4CQExEegoHbV', '4yIOlHpfiREBfJ', '4SFlePddQQh8OY'],
+                date: '2018-10-10',
+                park: 'Stazio',
                 scoreUs: 0,
                 scoreThem: 0,
                 lineupType: 2,
                 plateAppearances: [
                   {
-                    id: "4EhlxCjaM3VSUh",
-                    player_id: "4f4CQExEegoHbV",
+                    id: '4EhlxCjaM3VSUh',
+                    player_id: '4f4CQExEegoHbV',
                     location: {
                       x: 12141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "Out"
+                    result: 'Out',
                   },
                   {
-                    id: "4GgpbT2BIAykck",
-                    player_id: "4yIOlHpfiREBfJ",
+                    id: '4GgpbT2BIAykck',
+                    player_id: '4yIOlHpfiREBfJ',
                     location: {
                       x: 12141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "2B"
+                    result: '2B',
                   },
                   {
-                    id: "4HN7dirqjZvrXQ",
-                    player_id: "4SFlePddQQh8OY",
+                    id: '4HN7dirqjZvrXQ',
+                    player_id: '4SFlePddQQh8OY',
                     location: {
                       x: 12141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "1B"
-                  }
-                ]
-              }
-            ]
+                    result: '1B',
+                  },
+                ],
+              },
+            ],
           },
           {
-            id: "4W78nKNVldoDZ4",
-            name: "Just for fun",
-            games: []
+            id: '4W78nKNVldoDZ4',
+            name: 'Just for fun',
+            games: [],
           },
           {
-            id: "4fKFOTF7Wn4WIa",
-            name: "Pizza Team",
+            id: '4fKFOTF7Wn4WIa',
+            name: 'Pizza Team',
             games: [
               {
-                id: "4hHxL4AhHvS5El",
-                opponent: "Pizza Game",
-                lineup: ["47guOwUQN5QLPc", "4ZqtZbpYtlxOVW", "4ePGkCgKNTSnH4"],
-                date: "2018-10-10",
-                park: "Stazio",
+                id: '4hHxL4AhHvS5El',
+                opponent: 'Pizza Game',
+                lineup: ['47guOwUQN5QLPc', '4ZqtZbpYtlxOVW', '4ePGkCgKNTSnH4'],
+                date: '2018-10-10',
+                park: 'Stazio',
                 scoreUs: 0,
                 scoreThem: 0,
                 lineupType: 1,
                 plateAppearances: [
                   {
-                    id: "4FAKJaKkpvUGwv",
-                    player_id: "47guOwUQN5QLPc",
+                    id: '4FAKJaKkpvUGwv',
+                    player_id: '47guOwUQN5QLPc',
                     location: {
                       x: 12141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "1B"
+                    result: '1B',
                   },
                   {
-                    id: "4yIffY8qsCpNr9",
-                    player_id: "4ZqtZbpYtlxOVW",
+                    id: '4yIffY8qsCpNr9',
+                    player_id: '4ZqtZbpYtlxOVW',
                     location: {
                       x: 13141,
-                      y: 22141
+                      y: 22141,
                     },
-                    result: "2B"
+                    result: '2B',
                   },
                   {
-                    id: "4UvsHLN0x3MQIG",
-                    player_id: "4ePGkCgKNTSnH4",
+                    id: '4UvsHLN0x3MQIG',
+                    player_id: '4ePGkCgKNTSnH4',
                     location: {
                       x: 12141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "3B"
+                    result: '3B',
                   },
                   {
-                    id: "4VNCsBleBuQJCy",
-                    player_id: "47guOwUQN5QLPc",
+                    id: '4VNCsBleBuQJCy',
+                    player_id: '47guOwUQN5QLPc',
                     location: {
                       x: 123,
-                      y: 23515
+                      y: 23515,
                     },
-                    result: "Out"
+                    result: 'Out',
                   },
                   {
-                    id: "4LBFbOLR3Ri7xC",
-                    player_id: "4ZqtZbpYtlxOVW",
+                    id: '4LBFbOLR3Ri7xC',
+                    player_id: '4ZqtZbpYtlxOVW',
                     location: {
                       x: 12141,
-                      y: 19141
+                      y: 19141,
                     },
-                    result: "HRo"
+                    result: 'HRo',
                   },
                   {
-                    id: "4ncOGowVfcNET3",
-                    player_id: "4ePGkCgKNTSnH4",
+                    id: '4ncOGowVfcNET3',
+                    player_id: '4ePGkCgKNTSnH4',
                     location: {
                       x: 22141,
-                      y: 12141
+                      y: 12141,
                     },
-                    result: "1B"
-                  }
-                ]
+                    result: '1B',
+                  },
+                ],
               },
               {
-                id: "4M6UuAFJsfQukP",
-                opponent: "Another Game",
+                id: '4M6UuAFJsfQukP',
+                opponent: 'Another Game',
                 lineup: [],
-                date: "2018-10-10",
-                park: "Stazio",
+                date: '2018-10-10',
+                park: 'Stazio',
                 scoreUs: 0,
                 scoreThem: 0,
                 lineupType: 2,
-                plateAppearances: []
-              }
-            ]
-          }
-        ]
-      }
+                plateAppearances: [],
+              },
+            ],
+          },
+        ],
+      },
     };
   }
 
@@ -276,9 +279,9 @@ let databaseCalls = class DatabaseCalls {
 
   async patchState(patch, accountId) {
     if (accountId === undefined) {
-      throw new HandledError(403, "Please sign in first");
+      throw new HandledError(403, 'Please sign in first');
     }
-    console.log("Attempting to merge", this.STATES[accountId], patch, "Pizza");
+    logger.log('Attempting to merge', this.STATES[accountId], patch);
     objectMerge.patch(this.STATES[accountId], patch);
   }
 
@@ -289,7 +292,7 @@ let databaseCalls = class DatabaseCalls {
       email: email,
       password_hash: passwordHash, // pizza
       password_token_hash: passwordTokenHash,
-      password_token_expiration: Date.now() + 3600000
+      password_token_expiration: Date.now() + 3600000,
     };
     this.ACCOUNTS.push(newAccount);
     this.idCounter++;
@@ -297,7 +300,7 @@ let databaseCalls = class DatabaseCalls {
   }
 
   async getAccountFromTokenHash(passwordTokenHash) {
-    logger.log(null, "Seraching for", passwordTokenHash.trim());
+    logger.log(null, 'Seraching for', passwordTokenHash.trim());
     for (let i = 0; i < this.ACCOUNTS.length; i++) {
       if (this.ACCOUNTS[i].passwordTokenHash === passwordTokenHash) {
         if (this.ACCOUNTS[i].passwordTokenExpiration > Date.now()) {
@@ -341,7 +344,7 @@ let databaseCalls = class DatabaseCalls {
   }
 
   async deleteAccount(accountId) {
-    logger.log(accountId, "deleting");
+    logger.log(accountId, 'deleting');
     let indexToRemove = undefined;
     for (let i = 0; i < this.ACCOUNTS.length; i++) {
       if (this.ACCOUNTS[i].id === accountId) {
@@ -353,11 +356,14 @@ let databaseCalls = class DatabaseCalls {
   }
 
   async getNumberOfOptimizationsInProgress(accountId) {
-    logger.log(accountId, "getting optimization in progress count");
+    logger.log(accountId, 'getting optimization in progress count');
     let state = this.STATES[accountId];
     let count = 0;
     for (let i = 0; i < state.optimizations.length; i++) {
-      if (status == 1 || status == 2) {
+      if (
+        state.optimizations[i].status == 1 ||
+        state.optimizations[i].status == 2
+      ) {
         count++;
       }
     }
@@ -370,39 +376,66 @@ let databaseCalls = class DatabaseCalls {
     newStatus,
     optionalMessage
   ) {
-    logger.log(accountId, "setting optimization status");
+    optimizationId = idUtils.serverIdToClientId(optimizationId);
+    logger.log(
+      accountId,
+      'setting optimization status',
+      newStatus,
+      optionalMessage
+    );
     let state = this.STATES[accountId];
     for (let i = 0; i < state.optimizations.length; i++) {
       if (state.optimizations[i].id === optimizationId) {
         state.optimizations[i].status = newStatus;
         if (optionalMessage !== undefined) {
-          state.optimizations[i].status_message = optionalMessage;
+          state.optimizations[i].statusMessage = optionalMessage;
         } else {
-          state.optimizations[i].status_message = null;
+          state.optimizations[i].statusMessage = null;
         }
-        break;
+        return;
       }
     }
+    throw new Error(
+      'Optimization not found 5 ' +
+        optimizationId +
+        ' ' +
+        JSON.stringify(state.optimizations)
+    );
   }
 
   async setOptimizationResultData(accountId, optimizationId, newResults) {
-    logger.log(accountId, "getting optimization result data");
+    optimizationId = idUtils.serverIdToClientId(optimizationId);
+    logger.log(accountId, 'setting optimization result data', newResults);
     let state = this.STATES[accountId];
     for (let i = 0; i < state.optimizations.length; i++) {
       if (state.optimizations[i].id === optimizationId) {
-        state.optimizations[i].resultsData = newResults;
+        state.optimizations[i].resultData = newResults;
+        return;
       }
     }
+    throw new Error(
+      'Optimization not found 4 ' +
+        optimizationId +
+        ' ' +
+        JSON.stringify(state.optimizations)
+    );
   }
 
   async getOptimizationResultData(accountId, optimizationId) {
-    logger.log(accountId, "setting optimization result data");
+    optimizationId = idUtils.serverIdToClientId(optimizationId);
+    logger.log(accountId, 'getting optimization result data');
     let state = this.STATES[accountId];
     for (let i = 0; i < state.optimizations.length; i++) {
       if (state.optimizations[i].id === optimizationId) {
-        return state.optimizations[i].resultsData;
+        return state.optimizations[i].resultData;
       }
     }
+    throw new Error(
+      'Optimization not found 3 ' +
+        optimizationId +
+        ' ' +
+        JSON.stringify(state.optimizations)
+    );
   }
 
   async setOptimizationExecutionData(
@@ -410,24 +443,48 @@ let databaseCalls = class DatabaseCalls {
     optimizationId,
     newExecutionData
   ) {
-    logger.log(accountId, "setting optimization execution data");
+    optimizationId = idUtils.serverIdToClientId(optimizationId);
+    logger.log(
+      accountId,
+      'setting optimization execution data',
+      optimizationId,
+      newExecutionData
+    );
     let state = this.STATES[accountId];
     for (let i = 0; i < state.optimizations.length; i++) {
       if (state.optimizations[i].id === optimizationId) {
         state.optimizations[i].executionData = newExecutionData;
-        break;
+        return;
       }
     }
+    throw new Error(
+      'Optimization not found 2 ' +
+        optimizationId +
+        ' ' +
+        JSON.stringify(state.optimizations)
+    );
   }
 
   async getOptimizationExecutionData(accountId, optimizationId) {
-    logger.log(accountId, "setting optimization execution data");
+    optimizationId = idUtils.serverIdToClientId(optimizationId);
+
+    logger.log(
+      accountId,
+      'getting optimization execution data',
+      optimizationId
+    );
     let state = this.STATES[accountId];
     for (let i = 0; i < state.optimizations.length; i++) {
       if (state.optimizations[i].id === optimizationId) {
         return state.optimizations[i].executionData;
       }
     }
+    throw new Error(
+      'Optimization not found 1 ' +
+        optimizationId +
+        ' ' +
+        JSON.stringify(state.optimizations)
+    );
   }
 };
 
