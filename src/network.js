fb0534090d31df7175fe12439629d702e32887c6
Benjamin Brown <benjamin.t.brown@hotmail.com>
2022-07-31 21:07:01 -0600

    cleanup

--- src/network.js
+++ src/network.js
@@ -51,7 +51,7 @@ requestInternal = async function (
         });
         setTimeout(() => resolve(reqResp), NETWORK_DELAY);
       } catch (err) {
-        if (err.name == 'AbortError') {
+        if (err.name === 'AbortError') {
           console.log('Network request canceled');
           // Canceled Request (TODO: not sure the best way to handle this yet)
           resolve(-4); /*
