52258b70427f1adeca930631c83c1d5d7115d620
Thomas Brown <thomasaustenbrown@gmail.com>
2023-05-02 17:51:04 -0600

    Another test fix attempt

--- src-srv/test/jest-setup.js
+++ src-srv/test/jest-setup.js
@@ -1,70 +1,47 @@
 // Setup for jest tests - provide node implementations for browser apis
-if (global.localStorage === undefined) {
-  const configAccessor = require('../config-accessor.js');
 
-  // localstorage - https://stackoverflow.com/questions/32911630/how-do-i-deal-with-localstorage-in-jest-tests
-  class LocalStorageMock {
-    constructor() {
-      this.store = {};
-    }
+const configAccessor = require('../config-accessor.js');
 
-    clear() {
-      this.store = {};
-    }
-
-    getItem(key) {
-      return this.store[key] || null;
-    }
-
-    setItem(key, value) {
-      this.store[key] = value.toString();
-    }
-
-    removeItem(key) {
-      delete this.store[key];
-    }
+// localstorage - https://stackoverflow.com/questions/32911630/how-do-i-deal-with-localstorage-in-jest-tests
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
   }
 
-  global.localStorage = new LocalStorageMock();
-
-  // crypto - https://stackoverflow.com/questions/52612122/how-to-use-jest-to-test-functions-using-crypto-or-window-mscrypto
-  const crypto = require('crypto');
-  Object.defineProperty(global.self, 'crypto', {
-    value: {
-      // web crypto overrides the passed in array, node crypto returns a random array of the passed in length
-      getRandomValues: function (arr) {
-        let randomArray = new Uint8Array(crypto.randomBytes(arr.length));
-        for (let i = 0; i < arr.length; i++) {
-          arr[i] = randomArray[i];
-        }
-      },
-    },
-  });
-
-  // window location - origin is used by network.js
-  delete global.window.location;
-  global.window = Object.create(window);
-  global.window.location = {
-    origin: `http://localhost:${configAccessor.getAppServerPort()}`,
-  };
-
-  // Webworkers - we'll need to actually make this fully featured to test web workers
-  class MockWorker {
-    constructor(stringUrl) {
-      this.url = stringUrl;
-      this.onmessage = () => {};
-    }
-
-    postMessage(msg) {
-      //this.onmessage(msg);
-    }
-
-    terminate() {
-      // Okay... I terminated
-    }
+  clear() {
+    this.store = {};
   }
-  global.Worker = MockWorker;
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
 }
+
+if (global.localStorage === undefined) {
+  global.localStorage = new LocalStorageMock();
+}
+
+// crypto - https://stackoverflow.com/questions/52612122/how-to-use-jest-to-test-functions-using-crypto-or-window-mscrypto
+const crypto = require('crypto');
+Object.defineProperty(global.self, 'crypto', {
+  value: {
+    // web crypto overrides the passed in array, node crypto returns a random array of the passed in length
+    getRandomValues: function (arr) {
+      let randomArray = new Uint8Array(crypto.randomBytes(arr.length));
+      for (let i = 0; i < arr.length; i++) {
+        arr[i] = randomArray[i];
+      }
+    },
+  },
+});
 
 // authentication - allow tests to specify a sessionId that should be used for all fetch requests
 // There are two session IDs required for authentication. One is stored in an HTTPonly cookie to provide additional security against XXS.
@@ -87,3 +64,26 @@ global.fetch = function (url, opts) {
   let fetch = nodeFetch(url, opts);
   return fetch;
 };
+
+// window location - origin is used by network.js
+delete global.window.location;
+global.window.location = {
+  origin: `http://localhost:${configAccessor.getAppServerPort()}`,
+};
+
+// Webworkers - we'll need to actually make this fully featured to test web workers
+class MockWorker {
+  constructor(stringUrl) {
+    this.url = stringUrl;
+    this.onmessage = () => {};
+  }
+
+  postMessage(msg) {
+    //this.onmessage(msg);
+  }
+
+  terminate() {
+    // Okay... I terminated
+  }
+}
+global.Worker = MockWorker;
