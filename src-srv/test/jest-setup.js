2c04d5d33477a56e36c9d617964a32c4b90f3266
Thomas Brown <thomasaustenbrown@gmail.com>
2023-05-02 17:42:14 -0600

    Test another test fix

--- src-srv/test/jest-setup.js
+++ src-srv/test/jest-setup.js
@@ -41,28 +41,6 @@ if (global.localStorage === undefined) {
     },
   });
 
-  // authentication - allow tests to specify a sessionId that should be used for all fetch requests
-  // There are two session IDs required for authentication. One is stored in an HTTPonly cookie to provide additional security against XXS.
-  // The other is stored in a javascript accessible cookie and it's used so we can logout when the user is offline (which is not possible with HTTP only cookies).
-  let authCookies = null;
-  global.authenticate = function (inputAuthCookies) {
-    authCookies = inputAuthCookies;
-  };
-
-  // fetch - use node-fetch module but make sure to augment requests with headers auth info is included if specified
-  global.fetch = function (url, opts) {
-    if (authCookies) {
-      if (!opts.headers) {
-        opts.headers = {};
-      }
-      opts.headers.cookie = authCookies;
-    }
-    let nodeFetch = require('node-fetch');
-
-    let fetch = nodeFetch(url, opts);
-    return fetch;
-  };
-
   // window location - origin is used by network.js
   delete global.window.location;
   global.window = Object.create(window);
@@ -87,3 +65,25 @@ if (global.localStorage === undefined) {
   }
   global.Worker = MockWorker;
 }
+
+// authentication - allow tests to specify a sessionId that should be used for all fetch requests
+// There are two session IDs required for authentication. One is stored in an HTTPonly cookie to provide additional security against XXS.
+// The other is stored in a javascript accessible cookie and it's used so we can logout when the user is offline (which is not possible with HTTP only cookies).
+let authCookies = null;
+global.authenticate = function (inputAuthCookies) {
+  authCookies = inputAuthCookies;
+};
+
+// fetch - use node-fetch module but make sure to augment requests with headers auth info is included if specified
+global.fetch = function (url, opts) {
+  if (authCookies) {
+    if (!opts.headers) {
+      opts.headers = {};
+    }
+    opts.headers.cookie = authCookies;
+  }
+  let nodeFetch = require('node-fetch');
+
+  let fetch = nodeFetch(url, opts);
+  return fetch;
+};
